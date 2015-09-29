59eb89eb1cc76b1bea0aa46211ed87efb7382ddd
Sarah Binion <srhbinion@gmail.com>
2015-09-29 06:15:59 -0500

    adding collection details

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,7 +26,7 @@ bJams.config(function($stateProvider, $locationProvider) {
 		.state("collection",{
 			// properties of the state
 			url: "/collection",
-			controller:"CollectionController",
+			controller: "CollectionController",
 			templateUrl:"/templates/collection.html"
 		})
 		.state("album",{
@@ -37,32 +37,68 @@ bJams.config(function($stateProvider, $locationProvider) {
 		});
 });
 
+/**
+ * Controls the landing view
+ * @return {welcome}  - Hero Text
+ * @return {actionItems}  - Array with call-to-action items
+ */
 bJams.controller("LandingController", ["$scope", function($scope) {
-		$scope.welcome = "Turn the music up!";
-		$scope.point = {
-			actionItems:[
-				{
-					icon: "ion-iphone",
-					title: "Mobile enabled",
-					description: "Listen to your music on the go. This streaming service is available on all mobile platforms."
-				},
-				{
-					icon: "ion-radio-waves",
-					title: "Unlimited, streaming, ad-free",
-					description: "No arbitrary limits. No distractions."
-				},
-				
-				{
-					icon: "ion-music-note",
-					title: "Choose your music",
-					description: "The world is full of music; why should you have to listen to music that someone else chose?"
-				}
-			
-			]
-		} 	
+	$scope.welcome = "Turn the music up!";
+	$scope.point = {
+		actionItems:[
+			{
+				icon: "ion-iphone",
+				title: "Mobile enabled",
+				description: "Listen to your music on the go. This streaming service is available on all mobile platforms."
+			},
+			{
+				icon: "ion-radio-waves",
+				title: "Unlimited, streaming, ad-free",
+				description: "No arbitrary limits. No distractions."
+			},
+			{
+				icon: "ion-music-note",
+				title: "Choose your music",
+				description: "The world is full of music; why should you have to listen to music that someone else chose?"
+			}
+		]
+	};	
 }]);
 
 bJams.controller("CollectionController", ["$scope", function($scope) {
+	//texting purpose
+	$scope.albumArray = {
+        actionItems:[
+            {
+                name: "The Colors",
+                artist: "Pablo Picasso",
+                label: "Cubism",
+                year: "1881",
+                albumArtUrl: "assets/images/album_covers/01.png",
+                songs: [
+                    {name: "Blue", length: "4:26", audioUrl: "/assets/music/blue"},
+                    {name: "Green", length: "3:14", audioUrl: "assets/music/green"},
+                    {name: "Red", length: "5:01", audioUrl: "assets/music/red"},
+                    {name: "Pink", length: "3:21", audioUrl: "assets/music/pink"},
+                    {name: "Magenta", length: "2:15", audioUrl: "assets/music/magenta"}
+                ]
+            },
+            {
+                name: "The Colors",
+                artist: "Pablo Picasso",
+                label: "Cubism",
+                year: "1881",
+                albumArtUrl: "assets/images/album_covers/01.png",
+                songs: [
+                    {name: "Blue", length: "4:26", audioUrl: "/assets/music/blue"},
+                    {name: "Green", length: "3:14", audioUrl: "assets/music/green"},
+                    {name: "Red", length: "5:01", audioUrl: "assets/music/red"},
+                    {name: "Pink", length: "3:21", audioUrl: "assets/music/pink"},
+                    {name: "Magenta", length: "2:15", audioUrl: "assets/music/magenta"}
+                ]
+            }
+        ]
+    };
 }]);
 
 bJams.controller("AlbumController", ["$scope", function($scope) {
